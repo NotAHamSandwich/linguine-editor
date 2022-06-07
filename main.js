e64e648bc3b4dff8ace5efba7103f0993d4ea829
Quincy <quincythegodly@gmail.com>
2022-06-06 21:45:09 -0400

    update to main.js

--- main.js
+++ main.js
@@ -1,29 +1,153 @@
-const { BrowserWindow, app, dialog } = require("electron")
+const { BrowserWindow, app, dialog, Menu } = require("electron")
 const fs = require('fs');
+var ipc = require('electron').ipcMain;
 require("electron-reloader")(module);
 let open_File;
+let mainWindow;
 
 const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-        width: 900,
-        height: 700,
+    mainWindow = new BrowserWindow({
+        width: 1100,
+        height: 800,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+          enableRemoteModule: true,
+        },
     })
     mainWindow.loadFile("index.html")
+    mainWindow.openDevTools();
 };
+app.whenReady().then(createWindow);
 
-function openFile() {
-  const files = dialog.showOpenDialogSync( {
-    properties: ['openFile']})
 
-    if(!files) {
-      console.log("nope")
+ipc.on('open-file-dialog', (event) => {
+  dialog.showOpenDialog(mainWindow,
+    { properties: ['openFile', 'multiSelections']
+  }).then(result => {
+    if (result.canceled === false) {
+      event.reply("selected-file", result.filePaths)
+    }
+    else {
       return
     }
+  })
 
-    const file = files[0];
-    const fileContent = fs.readFileSync(file).toString();
-    open_File = fileContent
-    console.log(open_File);
-}
+})
 
-app.whenReady().then(createWindow);
+ipc.on('open-folder-dialog', (event) => {
+  dialog.showOpenDialog(mainWindow,
+    { properties: [ 'openDirectory', 'createDirectory' ]
+  }).then(result => {
+    if (result.canceled === false) {
+      event.reply("selected-folder", result)
+    }
+    else {
+      return
+    }
+  })
+})
+
+
+const isMac = process.platform === 'darwin'
+
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { type: 'separator' },
+      { role: 'save' },
+      { role: 'open file' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
